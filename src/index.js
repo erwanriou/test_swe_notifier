47de32b25e013a204b670b0df33b6a8d7de51c7d
erwanriou <rr.1.riou@gmail.com>
2026-01-18 14:42:51 +0100

    Start template notifier

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+// TODO const transaction = require("./services/transactions")
+
+// CONNECT DATABASE
+// TODO transaction("Notifier")
+
+// LISTEN APP
+app.listen(3000, () => console.log("Notifier listening on port 3000!"));
